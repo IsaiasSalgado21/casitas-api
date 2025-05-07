c69dc51eb3bbd2a8bddb136fe97cbfd86c095314
isaias salgado <isalgado_21@alu.uabcs.mx>
2025-05-07 00:13:41 -0700

    creamos el proyecto con laravel

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/** PERSONAL API TOKENS WITH SANCTUM! */
+
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () 
+{
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
