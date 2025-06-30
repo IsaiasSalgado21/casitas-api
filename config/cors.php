4fbcba58befa9b09a4219e57b9906b8c0fe0cd28
job moore <jobmurdan@ohtmail.com>
2025-06-30 15:08:43 -0700

    añadi mi ruta a la config del cors

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8100'], 
+    'allowed_origins' => ['http://localhost:8100', 'http://127.0.0.1:8100'],
 
     'allowed_origins_patterns' => [],
 
